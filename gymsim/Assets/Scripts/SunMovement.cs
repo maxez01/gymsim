df73b6759f4a459142db4c92f6dc4ad12eae84d5
maxez01 <m.zeband@gmail.com>
2022-11-22 16:44:26 +0100

    edit movement and prepare next steps

--- gymsim/Assets/Scripts/SunMovement.cs
+++ gymsim/Assets/Scripts/SunMovement.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class SunMovement : MonoBehaviour
 {
     public float theta = 0.01f;
+
+    public float xRot;
+    public float yRot;
     void Update()
     {
         // TODO change axis
         // TODO time dependent
-        this.transform.RotateAround(Vector3.zero, Vector3.right, theta);
+        // TODO realistic with max in y and 4 versions for each season
+        this.transform.RotateAround(Vector3.zero, new Vector3(xRot, -yRot ,0), theta);
     }
 }
