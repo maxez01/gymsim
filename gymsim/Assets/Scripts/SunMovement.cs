9672812780769afc1468a22793cfbc933dc977f9
Conrad Strassburger <98957100+conradstrassburger@users.noreply.github.com>
2022-11-24 00:18:35 +0100

    Add TextMeshPro and use it for a basic HUD

--- gymsim/Assets/Scripts/SunMovement.cs
+++ gymsim/Assets/Scripts/SunMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class SunMovement : MonoBehaviour
 {
@@ -8,11 +9,20 @@ public class SunMovement : MonoBehaviour
 
     public float xRot;
     public float yRot;
+
+    public GameObject timeText;
+
     void Update()
     {
         // TODO change axis
         // TODO time dependent
         // TODO realistic with max in y and 4 versions for each season
         this.transform.RotateAround(Vector3.zero, new Vector3(xRot, -yRot ,0), theta);
+
+        TextMeshProUGUI text = timeText.GetComponent<TextMeshProUGUI>();
+        float angle = 0.0f;
+        Vector3 axis = Vector3.zero;
+        this.transform.rotation.ToAngleAxis(out angle, out axis);
+        text.text = "Sonnenwinkel: " + angle + "°";
     }
 }
