f86a0ae5aae6d5ff9e71a447154bf3caafe23667
maxez01 <m.zeband@gmail.com>
2022-11-25 13:18:29 +0100

    modify Fitnessstudio.blend, modify Camera and SunMovement.cs

--- gymsim/Assets/Scripts/SunMovement.cs
+++ gymsim/Assets/Scripts/SunMovement.cs
@@ -2,17 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+enum Season
+{
+    Spring = 1,
+    Summer = 2,
+    Autumn = 3,
+    Winter = 4
+}
+
 public class SunMovement : MonoBehaviour
 {
-    public float theta = 0.01f;
+    public int season; 
+    public float speed = 0.01f;
 
     public float xRot;
     public float yRot;
+
     void Update()
     {
         // TODO change axis
         // TODO time dependent
+
         // TODO realistic with max in y and 4 versions for each season
-        this.transform.RotateAround(Vector3.zero, new Vector3(xRot, -yRot ,0), theta);
+        switch(season) {
+            case (int)Season.Spring: 
+                break;
+            case (int)Season.Summer: 
+                break;
+            case (int)Season.Autumn: 
+                break;
+            case (int)Season.Winter: 
+                break;
+        }
+        this.transform.Rotate(new Vector3(xRot * Time.deltaTime, -yRot * Time.deltaTime ,0), speed);
     }
 }
