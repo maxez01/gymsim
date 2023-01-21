5e6aa5c63a7467f4785c79310f274fa37ab935b7
maxez01 <m.zeband@gmail.com>
2023-01-21 18:05:35 +0100

    reimport Butterfly and Rudermaschine | add animated objects into scene | add initializeGameObjects to MachineManager

--- gymsim/Assets/Scripts/MachineManager.cs
+++ gymsim/Assets/Scripts/MachineManager.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 
 public class MachineManager : MonoBehaviour
 {
-    private Dictionary<int, Machine> machines;
+    private Dictionary<int, Machine> unanimatedMachines;
+    private Dictionary<int, Machine> animatedMachines;
     // Start is called before the first frame update
     void Start()
     {
-        machines = new Dictionary<int, Machine>();
+        initializeGameObjects();
     }
 
     // Update is called once per frame
@@ -18,9 +19,51 @@ public class MachineManager : MonoBehaviour
         
     }
 
+    private void initializeGameObjects() {
+        int k = 0;
+        unanimatedMachines = new Dictionary<int, Machine>();
+        animatedMachines = new Dictionary<int, Machine>();        
+
+        // TODO only hardcoded now, could use better code style
+        string[] machineListNames = new string[] {"Laufband", "Butterfly", "Brustpresse", "Rudermaschine_", "Beinpresse", "Kurzhantel"};
+        int[] machineListCount = new int[] {4, 3, 2, 3, 3, 2};
+
+        for(int i = 0; i < machineListNames.Length; i++) {
+            for (int j = 0; j < machineListCount[i]; j++) {
+                Machine unanimatedMachine = new Machine();
+                Machine animatedMachine = new Machine();
+
+                if (j > 0) {
+                    unanimatedMachine.SetGameObject(GameObject.Find("Geräte/" + machineListNames[i] + " (" + j + ")"));
+                    animatedMachine.SetGameObject(GameObject.Find("Geräte/" + machineListNames[i] + " - animated (" + j + ")"));
+                } else {
+                    unanimatedMachine.SetGameObject(GameObject.Find("Geräte/" + machineListNames[i]));
+                    animatedMachine.SetGameObject(GameObject.Find("Geräte/" + machineListNames[i] + " - animated"));
+                }
+
+                animatedMachine.gameObject.SetActive(false);
+                
+                unanimatedMachines.Add(k, unanimatedMachine);
+                animatedMachines.Add(k, animatedMachine);
+
+                k++;
+
+                // enable if neccessary
+
+                // Debug.Log(unanimatedMachines.GetValueOrDefault(k-1).gameObject.name);
+                // Debug.Log(animatedMachines.GetValueOrDefault(k-1).gameObject.name);
+            }
+        }
+    }
+
     private class Machine
     {
         public string name;
         public Boolean isUsed;
+        public GameObject gameObject;
+
+        public void SetGameObject(GameObject value) {
+            gameObject = value;
+        }
     }
 }
