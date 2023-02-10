86cf3990309438b55c09e5d94a696daf81041c14
Conrad Strassburger <98957100+conradstrassburger@users.noreply.github.com>
2023-02-10 23:24:35 +0100

    Unpack Studio object and refine head movement

--- gymsim/Assets/Scripts/HeadMovement.cs
+++ gymsim/Assets/Scripts/HeadMovement.cs
@@ -5,23 +5,24 @@ using UnityEngine;
 public class HeadMovement : MonoBehaviour
 {
     private AIMovement aiMovement;
-    private float wallX;
+    private Bounds fitnessStudio;
 
     // Start is called before the first frame update
     void Start()
     {
-        wallX = GameObject.Find("wall_right").transform.position.x;
+        fitnessStudio = GameObject.Find("Fitnessstudio").GetComponent<MeshRenderer>().bounds;
         aiMovement = GameObject.Find("npcs").GetComponent<AIMovement>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        AIMovement.NPC walkingNPC = aiMovement.npcs.Find(npc => npc.state == AIMovement.NPCState.walkingIn && npc.gameObject.transform.position.x > wallX 
-        || npc.state == AIMovement.NPCState.walkingOut && npc.gameObject.transform.position.x > wallX);
+        AIMovement.NPC walkingNPC = aiMovement.npcs.Find(npc => (npc.state == AIMovement.NPCState.walkingIn || npc.state == AIMovement.NPCState.walkingOut) && fitnessStudio.Contains(npc.gameObject.transform.position));
 
-        if (walkingNPC != null)
+        if (walkingNPC != null) 
+        {
             transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(walkingNPC.gameObject.transform.position - transform.position), Time.deltaTime * 10);
+        }
         else
             transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(Vector3.zero), Time.deltaTime * 10);
     }
