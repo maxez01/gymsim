5060f6621733a25144a55640e49da5b03eb3850d
Conrad Strassburger <98957100+conradstrassburger@users.noreply.github.com>
2023-02-11 01:00:54 +0100

    Make counter NPC look at random training NPCs

--- gymsim/Assets/Scripts/HeadMovement.cs
+++ gymsim/Assets/Scripts/HeadMovement.cs
@@ -6,6 +6,13 @@ public class HeadMovement : MonoBehaviour
 {
     private AIMovement aiMovement;
     private Bounds fitnessStudio;
+    private float lookAwayTimeEnd = 0;
+    private MachineManager.Machine machineToLookAt;
+    private bool wantsToLook;
+
+    private static readonly float lookProbability = 0.5f;
+    private static readonly float rotationSpeed = 7;
+    private static readonly RangeInt lookAwayTimeRange = new(2, 1);
 
     // Start is called before the first frame update
     void Start()
@@ -19,11 +26,31 @@ public class HeadMovement : MonoBehaviour
     {
         AIMovement.NPC walkingNPC = aiMovement.npcs.Find(npc => (npc.state == AIMovement.NPCState.walkingIn || npc.state == AIMovement.NPCState.walkingOut) && fitnessStudio.Contains(npc.gameObject.transform.position));
 
-        if (walkingNPC != null) 
+        // looking at walking people has top priority
+        if (walkingNPC != null)
         {
-            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(walkingNPC.gameObject.transform.position - transform.position), Time.deltaTime * 10);
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(walkingNPC.gameObject.transform.position - transform.position), Time.deltaTime * rotationSpeed);
+            lookAwayTimeEnd = 0;
         }
         else
-            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(Vector3.zero), Time.deltaTime * 10);
+        {
+            List<AIMovement.NPC> workingOutNPCs = aiMovement.npcs.FindAll(npc => npc.state == AIMovement.NPCState.workingOut);
+
+            if (workingOutNPCs.Count != 0 && Time.time > lookAwayTimeEnd)
+            {
+                lookAwayTimeEnd = Time.time + Random.Range(lookAwayTimeRange.start, lookAwayTimeRange.end);
+                machineToLookAt = aiMovement.machineManager.animatedMachines[workingOutNPCs[Random.Range(0, workingOutNPCs.Count - 1)].machineKey];
+                wantsToLook = Random.value > lookProbability;
+            }
+
+            if (wantsToLook)
+            {
+                transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(machineToLookAt.gameObject.transform.position - transform.position), Time.deltaTime * rotationSpeed);
+            }
+            else
+            {
+                transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(Vector3.zero), Time.deltaTime * rotationSpeed);
+            }
+        }
     }
 }
